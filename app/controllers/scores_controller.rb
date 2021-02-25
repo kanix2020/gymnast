b7aa383f14662c15145f41d56e2f137b8ed939d6
kanix2020 <tuno1yngw@gmail.com>
2021-02-25 18:26:00 +0900

    csv（少数２まで表示）

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -46,14 +46,14 @@ class ScoresController < ApplicationController
 
   def send_scores_csv(scores)
     csv_data = CSV.generate(row_sep: "\r\n", encoding:Encoding::CP932)  do |csv|
-      header = %w(name - FX PH Ri Vo PB HB Total)
+      header = %w(name － FX PH Ri Vo PB HB Total)
       csv << header
       scores.each do |score|
         values_d = [score.athlete.name,"D", score.floor_d, score.pommel_d, score.rings_d, score.vault_d, score.parallel_d, score.horizontal_d]
         values_e = ["", "E", score.floor_e, score.pommel_e, score.rings_e, score.vault_e,score.parallel_e, score.horizontal_e]
         values_nd = ["", "ND", score.floor_nd, score.pommel_nd, score.rings_nd, score.vault_nd, score.parallel_nd, score.horizontal_nd]
-        values_total = ["","-",(score.floor_d + score.floor_e - score.floor_nd),(score.pommel_d + score.pommel_e - score.pommel_nd),(score.rings_d + score.rings_e - score.rings_nd),(score.vault_d + score.vault_e - score.vault_nd),(score.parallel_d + score.parallel_e - score.parallel_nd),(score.horizontal_d + score.horizontal_e - score.horizontal_nd),
-        (score.floor_d + score.floor_e - score.floor_nd) + (score.pommel_d + score.pommel_e - score.pommel_nd) + (score.rings_d + score.rings_e - score.rings_nd) + (score.vault_d + score.vault_e - score.vault_nd) + (score.parallel_d + score.parallel_e - score.parallel_nd) + (score.horizontal_d + score.horizontal_e - score.horizontal_nd)]
+        values_total = ["","－",(score.floor_d + score.floor_e - score.floor_nd).floor(2),(score.pommel_d + score.pommel_e - score.pommel_nd).floor(2),(score.rings_d + score.rings_e - score.rings_nd).floor(2),(score.vault_d + score.vault_e - score.vault_nd).floor(2),(score.parallel_d + score.parallel_e - score.parallel_nd).floor(2),(score.horizontal_d + score.horizontal_e - score.horizontal_nd).floor(2),
+        ((score.floor_d + score.floor_e - score.floor_nd) + (score.pommel_d + score.pommel_e - score.pommel_nd) + (score.rings_d + score.rings_e - score.rings_nd) + (score.vault_d + score.vault_e - score.vault_nd) + (score.parallel_d + score.parallel_e - score.parallel_nd) + (score.horizontal_d + score.horizontal_e - score.horizontal_nd)).floor(2)]
 
         csv << values_d
         csv << values_e
