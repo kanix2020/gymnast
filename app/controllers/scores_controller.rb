7ccdeeaddba1e1be420cd13c8fee4346386b5319
kanix2020 <tuno1yngw@gmail.com>
2021-02-25 16:30:31 +0900

    total追加（csv）

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -46,21 +46,22 @@ class ScoresController < ApplicationController
 
   def send_scores_csv(scores)
     csv_data = CSV.generate(row_sep: "\r\n", encoding:Encoding::CP932)  do |csv|
-      header = %w(name / FX PH Ri Vo PB HB 計算欄)
+      header = %w(name - FX PH Ri Vo PB HB Total)
       csv << header
       scores.each do |score|
-        # values_event = ["","FX","PH","Ri","Vo","PB","HB"]
         values_d = [score.athlete.name,"D", score.floor_d, score.pommel_d, score.rings_d, score.vault_d, score.parallel_d, score.horizontal_d]
         values_e = ["", "E", score.floor_e, score.pommel_e, score.rings_e, score.vault_e,score.parallel_e, score.horizontal_e]
         values_nd = ["", "ND", score.floor_nd, score.pommel_nd, score.rings_nd, score.vault_nd, score.parallel_nd, score.horizontal_nd]
-        # csv << values_event
+        values_total = ["","-",(score.floor_d + score.floor_e - score.floor_nd),(score.pommel_d + score.pommel_e - score.pommel_nd),(score.rings_d + score.rings_e - score.rings_nd),(score.vault_d + score.vault_e - score.vault_nd),(score.parallel_d + score.parallel_e - score.parallel_nd),(score.horizontal_d + score.horizontal_e - score.horizontal_nd),
+        (score.floor_d + score.floor_e - score.floor_nd) + (score.pommel_d + score.pommel_e - score.pommel_nd) + (score.rings_d + score.rings_e - score.rings_nd) + (score.vault_d + score.vault_e - score.vault_nd) + (score.parallel_d + score.parallel_e - score.parallel_nd) + (score.horizontal_d + score.horizontal_e - score.horizontal_nd)]
+
         csv << values_d
         csv << values_e
         csv << values_nd
+        csv << values_total
       end
     end
     send_data(csv_data, filename: "#{@game.name}.csv")
-
   end
 
 end
